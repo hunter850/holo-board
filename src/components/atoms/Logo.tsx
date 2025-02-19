19b916cda7d45bc9eaed00c8cc43a821278c9a0b
hunter850 <hunter850711@gmail.com>
2025-02-19 23:59:19 +0800

    [Feat] add link for Logo to redirect to homepage

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Logo() {
     return (
-        <h1 className="text-2xl font-bold">
-            <span className="text-primary">Holo</span>Board
-        </h1>
+        <Link href="/">
+            <div className="flex cursor-pointer items-center gap-2">
+                <h1 className="text-2xl font-bold">
+                    <span className="text-primary">Holo</span>Board
+                </h1>
+            </div>
+        </Link>
     );
 }
