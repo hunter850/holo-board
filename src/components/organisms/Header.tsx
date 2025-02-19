f90f34ea59afec3583fd2962049786fa9101e680
hunter850 <hunter850711@gmail.com>
2025-02-20 00:00:12 +0800

    [Update] hide google login button

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,16 +1,16 @@
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import Logo from "@/components/atoms/Logo";
-import LoginButton from "@/components/molecules/LoginButton";
-import LoginButtonSkeleton from "@/components/molecules/LoginButtonSkeleton";
+// import LoginButton from "@/components/molecules/LoginButton";
+// import LoginButtonSkeleton from "@/components/molecules/LoginButtonSkeleton";
 
 export default function Header() {
     return (
-        <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky left-0 right-0 top-0 z-50 h-16 border-b backdrop-blur">
+        <header className="sticky left-0 right-0 top-0 z-50 h-16 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container mx-auto flex h-full items-center justify-between px-4">
                 <Logo />
-                <Suspense fallback={<LoginButtonSkeleton />}>
+                {/* <Suspense fallback={<LoginButtonSkeleton />}>
                     <LoginButton />
-                </Suspense>
+                </Suspense> */}
             </div>
         </header>
     );
