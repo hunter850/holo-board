ae032e24bacc39bc5b36eee4de5bedf16b53253c
hunter850 <hunter850711@gmail.com>
2025-02-20 01:00:08 +0800

    [Feat] add app icon & update metadata

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Logo() {
     return (
         <Link href="/">
             <div className="flex cursor-pointer items-center gap-2">
+                <Image src="/images/hd_icon.png" alt="HoloBoard Logo" width={48} height={48} />
                 <h1 className="text-2xl font-bold">
                     <span className="text-primary">Holo</span>Board
                 </h1>
